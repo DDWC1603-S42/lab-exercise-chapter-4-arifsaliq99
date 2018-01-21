650680bb3d742d53eda5b3d49fef97a5b595046d
arifsaliq99 <35547940+arifsaliq99@users.noreply.github.com>
2018-01-21 17:59:05 +0800

    Update Lab-Exercise-5.cpp

--- Lab-Exercise-5.cpp
+++ Lab-Exercise-5.cpp
@@ -1,21 +1,22 @@
-//Get out from this program
-//find a way to fix and complete this program. good luck.
 
 #include <iostream>
 using namespace std;
 int main()
 {
+	cout<<"Enter 0 to exit or anyhing else to continue: ";
+		int num;
+		cin>>num;
 	while(false)//infinite loop
 	{
-		std::cout<<"Enter 0 to exit or anyhing else to continue: ";
-		int num;
-		cin>>num;	
+		
 
-		//exit loop if user enters
-		if(num==0)
-			break;
+		if(num==0){
+		
+	
+			break;}
+			
+			
 	}
-	std::cout<<"we are out";
-
+     cout<<"we are out";
 	return 0;
 }
